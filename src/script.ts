e3f76eaee9d8752879a45fa779d819b325ee9a1b
Michael Ashe <mickjohnashe@hotmail.com>
2018-11-08 18:07:21 +0000

    Disabling the login button after the first click. This prevents multiple loggin requests being sent. Adding 'Connecting...' message showing that the webpage is doing something after you click the login button

--- src/script.ts
+++ src/script.ts
@@ -4,7 +4,7 @@ import { MessageHandler } from './handler';
 
 var username_field = $("username");
 var current_hostname: string = window.location.hostname;
-var websocket_url: string = "ws://" + current_hostname + ":8000";
+var websocket_url: string;
 if ( current_hostname === "games.mickjohn.com" ) {
   websocket_url = "wss://" + current_hostname + ":8010";
 } else {
@@ -33,6 +33,7 @@ $(document).ready(function(){
   $("#your-go").hide();
 
   $("#login").click( function() {
+    $(this).prop('disabled', true);
     var username: string = $("#username").val().toString();
     startWebsocketConnection(username);
   });
@@ -42,8 +43,18 @@ $(document).ready(function(){
   });
 });
 
+// Reset back to the starting state
+// i.e. game is hidden and login box is
+// showing
+function resetGameUi() {
+    $('#game-div').hide();
+    $('#login-div').show();
+    $('#login').prop('disabled', false);
+}
+
 function startWebsocketConnection(username: string) {
   console.log("About to create websocket connection...");
+  $('#login-messages').text("Connecting...");
   var connection: WebSocket = new WebSocket(websocket_url);
   var username: string;
   var handler: MessageHandler;
@@ -55,6 +66,7 @@ function startWebsocketConnection(username: string) {
     handler = new MessageHandler(username, connection);
     connection.send(JSON.stringify(new Login(username))); 
     $("#login-div").hide();
+    $('#login-messages').text("");
   };
 
   connection.onmessage = function(event) {
@@ -63,14 +75,12 @@ function startWebsocketConnection(username: string) {
   };
 
   connection.onclose = function(event) {
-    console.log("Lost websocket connection");
-    $('#game-div').hide();
-    $('#login-div').show();
+    console.log("Websocket connection closed");
+    resetGameUi();
   }
 
   connection.onerror = function(event) {
     console.log("websocket error!");
-    $('#game-div').hide();
-    $('#login-div').show();
+    resetGameUi();
   }
 }
